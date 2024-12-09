902d99fc2ec6b1a050c8d944be48696c4c56b8ff
Szymon Miśtal <mistalzaq@gmail.com>
2024-12-09 18:24:43 +0100

    Solve day 6 part 2

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -29,11 +30,23 @@ func ParseGrid(input []string) (Grid, Guard) {
 }
 
 func (g Grid) Get(x int, y int) rune {
-	if x < 0 || x >= len(g[0]) || y < 0 || y >= len(g) {
+	if !g.InBounds(x, y) {
 		return 0
 	}
 
 	return g[y][x]
+}
+
+func (g Grid) InBounds(x int, y int) bool {
+	if x < 0 || x >= len(g[0]) || y < 0 || y >= len(g) {
+		return false
+	}
+
+	return true
+}
+
+func (g *Grid) Set(x int, y int, char rune) {
+	(*g)[y][x] = char
 }
 
 func (g Grid) Visit(x int, y int) {
@@ -46,6 +59,47 @@ func (g Grid) Print() {
 			fmt.Printf("%s", string(char))
 		}
 		fmt.Println()
+	}
+}
+
+func (g Grid) Simulate(guard *Guard) bool {
+	// start := Guard{guard.x, guard.y, guard.direction}
+	occurences := make(map[string]int)
+
+	for guard.InBounds(g) {
+		str := fmt.Sprintf("%d,%d,%d,%d", guard.x, guard.y, guard.direction.x, guard.direction.y)
+		if guard.InBounds(g) {
+			if occurences[str] += 1; occurences[str] > 1 {
+				return true
+			}
+		}
+		guard.Move(g)
+		// if guard.x == start.x && guard.y == start.y && guard.direction.x == start.direction.x && guard.direction.y == start.direction.y {
+		// return true
+		// }
+	}
+
+	return false
+}
+
+func (g Grid) SimulateWithObstacle(guard Guard, obstacle Vec) bool {
+	if g.Get(obstacle.x, obstacle.y) == '#' || !g.InBounds(obstacle.x, obstacle.y) {
+		return false
+	}
+
+	g.Set(obstacle.x, obstacle.y, '#')
+	loop := g.Simulate(&guard)
+	g.Set(obstacle.x, obstacle.y, '.')
+	return loop
+}
+
+func (g *Grid) Clear() {
+	for y := range len(*g) {
+		for x := range len((*g)[y]) {
+			if (*g)[y][x] != '#' {
+				(*g)[y][x] = '.'
+			}
+		}
 	}
 }
 
@@ -76,6 +130,10 @@ func (g Grid) CountVisitedFields() int {
 type Vec struct {
 	x int
 	y int
+}
+
+func (v Vec) AsString() string {
+	return fmt.Sprintf("%v,%v", v.x, v.y)
 }
 
 type Guard struct {
@@ -126,13 +184,34 @@ func main() {
 func PartOne(input []string) int {
 	grid, guard := ParseGrid(input)
 
-	for guard.InBounds(grid) {
-		guard.Move(grid)
-	}
+	grid.Simulate(&guard)
 
 	return grid.CountVisitedFields()
 }
 
 func PartTwo(input []string) int {
-	return -1
+	grid, guard := ParseGrid(input)
+	x, y, dir := guard.x, guard.y, guard.direction
+
+	var states []Guard
+
+	for guard.InBounds(grid) {
+		x, y, dir := guard.x, guard.y, guard.direction
+		if !guard.Move(grid) {
+			states = append(states, Guard{x, y, dir})
+		}
+	}
+
+	var positions []string
+
+	for _, state := range states {
+		obstacle := Vec{state.x + state.direction.x, state.y + state.direction.y}
+		if !slices.Contains(positions, obstacle.AsString()) {
+			if grid.SimulateWithObstacle(Guard{x, y, dir}, obstacle) {
+				positions = append(positions, obstacle.AsString())
+			}
+		}
+	}
+
+	return len(positions)
 }
